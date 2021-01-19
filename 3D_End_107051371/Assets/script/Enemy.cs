45a6e9ecf6e1aa8aaf55842125ed10dd1eae0d03
icelabex <entg.1216lbex@gmail.com>
2021-01-19 12:53:12 +0800

    end

---
+++ 3D_End_107051371/Assets/script/Enemy.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using UnityEngine.AI;// use aicoode
+public class Enemy : MonoBehaviour
+{
+
+    [Header("MoveSpeed"), Range(0, 50)]
+    public float speed = 3;
+    [Header("StopDistance"), Range(0, 50)]
+    public float stopDistance = 2.5f;
+    [Header("AttackCoolTime"), Range(0, 50)]
+    public float ct = 2f;
+    [Header("AttackCenter")]
+    public Transform atkPoint;
+    [Header("Atklength"), Range(0f, 5f)]
+    public float atkLength;
+    [Header("Strength"), Range(0f, 500)]
+    public float atk = 30;
+
+    
+    private Transform player;
+    private NavMeshAgent nav;
+    private Animator ani;
+    //計測器
+    private float timer;
+
+    private void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+
+        ani = GetComponent<Animator>();
+
+        player = GameObject.Find("Player").transform;
+
+
+        nav.speed = speed;
+        nav.stoppingDistance = stopDistance;
+    }
+
+    private void Update()
+    {
+        Track();
+        Attack();
+    }
+
+    //線を描く
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(atkPoint.position, atkPoint.forward * atkLength);
+    }
+
+    private RaycastHit hit;
+
+    public float hp = 100;
+
+    private void Attack()
+    {
+        if(nav.remainingDistance < stopDistance)
+        {
+            timer += Time.deltaTime;
+            Vector3 pos = player.position;
+            pos.y = transform.position.y;
+
+            transform.LookAt(pos);
+            
+            if (timer >= ct)
+            {
+                ani.SetTrigger("attacksys");
+                timer = 0;
+                //物理の判断式
+                if (Physics.Raycast(atkPoint.position, atkPoint.forward,out hit,atkLength,1 << 8))
+                {
+                    //コードを呼び出してダメージのアニメーションを行う
+                    hit.collider.GetComponent<Player>().Damage(atk);
+                }
+            }
+        }
+    }
+
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        ani.SetTrigger("damagedsys");
+
+        if (hp <= 0)
+        {
+            Dead();
+        }
+    }
+
+    private void Dead()
+    {
+        nav.isStopped = true;
+        enabled = false;
+        ani.SetBool("deathysys",true);
+      
+    }
+
+    //ここでプレーヤーの位置をトラック
+    private void Track()
+    {
+        nav.SetDestination(player.position);
+        //距離でアニメーションをコントロール
+        ani.SetBool("runsys", nav.remainingDistance > stopDistance);
+    }
+}
---
+++ 3D_End_107051371/Assets/script/Player.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using Invector.vCharacterController;
+public class Player : MonoBehaviour
+{
+
+    private float hp = 100;
+    private Animator ani;
+
+    private int atkCount;
+
+    private float timer;
+
+    [Header("ComboCT"), Range(0, 3)]
+    public float interval = 1;
+    [Header("AttackCenter")]
+    public Transform atkPoint;
+    [Header("Atklength"), Range(0f, 5f)]
+    public float atkLength;
+    [Header("Strength"), Range(0f, 500)]
+    public float atk = 30;
+
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+        Attack();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(atkPoint.position, atkPoint.forward * atkLength);
+    }
+
+    private RaycastHit hit;
+
+    private void Attack()
+    {
+        if (atkCount < 3)
+        {
+            if (timer < interval)
+            {
+                timer += Time.deltaTime;
+
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    atkCount++;
+                    timer = 0;
+                    ani.SetInteger("Combo", atkCount);
+
+                    if (Physics.Raycast(atkPoint.position, atkPoint.forward, out hit, atkLength, 1 << 9))
+                    {
+                        //コードを呼び出してダメージのアニメーションを行う
+                        hit.collider.GetComponent<Enemy>().Damage(atk);
+                    }
+                }
+            }
+            else
+            {
+                atkCount = 0;
+                timer = 0;
+            }
+        }
+
+        if (atkCount == 3) atkCount = 0;
+        ani.SetInteger("Combo", atkCount);
+    }
+
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        ani.SetTrigger("Damagedsys");
+
+        if (hp <= 0)
+        {
+            Dead();
+        }
+    }
+
+    private void Dead()
+    {
+        ani.SetTrigger("Diesys");
+        vThirdPersonController vt = GetComponent<vThirdPersonController>();
+        vt.lockMovement = true;
+        vt.lockRotation = true;
+    }
+}
